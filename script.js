2d24e12dfc9965d9e15c302983e5f1c5ccd9b4fb
Britteany <miszbritteany@gmail.com>
2023-11-06 18:21:01 -0500

    created 3 objects

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//Gameboard object with board array
+const boardGame = (() => {
+    let board = [[,,],[,,],[,,]]
+});
+
+//Logic that will control the game flow
+const gameControl = (()=> {
+
+});
+
+//Player logic
+const players = (() => {
+
+}):
+
 const startButton = document.querySelector("#start");
 startButton.addEventListener("click",()=> {
     alert("Hello");
