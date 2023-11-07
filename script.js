69bca1a0b76ba982505cfb1458d8a4255d3fdeb3
Britteany <miszbritteany@gmail.com>
2023-11-06 20:27:50 -0500

    started game logic, added event listener to tiles

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 //Gameboard object with board array
 const boardGame = (() => {
-    let board = [[,,],[,,],[,,]]
-});
+    let board = [[, ,], [, ,], [, ,]]
 
-//Logic that will control the game flow
-const gameControl = (()=> {
+    const tiles = document.querySelectorAll(".tile");
+    tiles.forEach((tile) => {
+        tile.addEventListener("click", () => {
 
-});
+        })
+    })
+})();
 
 //Player logic
-const players = (() => {
+const createPlayers = (name, john) => {
+    return {
+        name,
+        john
+    }
+}
 
-}):
+//Logic that will control the game flow
+const gameControl = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
 
-const startButton = document.querySelector("#start");
-startButton.addEventListener("click",()=> {
-    alert("Hello");
-});
+    const startButton = document.querySelector("#start");
+    startButton.addEventListener("click", () => {
+        const startGame = () => {
+            players = [
+                createPlayers(document.querySelector("#player1").value, "X"),
+                createPlayers(document.querySelector("#player2").value, "O")
+            ]
+            currentPlayerIndex = 0;
+            gameOver = false;
+        }
+        return {
+            startGame
+        }
+    });
+    
+})();
 
 const resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click",()=> {
+resetButton.addEventListener("click", () => {
     alert("Hello");
 });
