6a223f7b3e8c85a8277e2f178029c86dc734b705
Britteany <miszbritteany@gmail.com>
2023-11-07 21:35:25 -0500

    prevent clicking in same spot

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ const boardGame = (() => {
             //update board array and visual side
             const row = Math.floor(index / 3);
             const col = index % 3;
-            board[row][col] = currentPlayer.symbol;
-            tile.textContent = currentPlayer.symbol;
-            tile.classList.add(currentPlayer.symbol === "X" ? "x-symbol" : "o-symbol");
+            if (!board[row][col]) {
+                board[row][col] = currentPlayer.symbol;
+                tile.textContent = currentPlayer.symbol;
+                tile.classList.add(currentPlayer.symbol === "X" ? "x-symbol" : "o-symbol");
 
-            currentPlayer = player1Turn ? players[0] : players[1];
-            //show who's turn it is
-            console.log(`It's ${currentPlayer.name}'s turn (${currentPlayer.symbol})`);
-            //switch turns
-            player1Turn = !player1Turn;
+                currentPlayer = player1Turn ? players[0] : players[1];
+                //show who's turn it is
+                console.log(`It's ${currentPlayer.name}'s turn (${currentPlayer.symbol})`);
+                //switch turns
+                player1Turn = !player1Turn;
+            }
         })
     })
 })();
