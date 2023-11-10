46a0e84552e12dade6234a6dace16ce7c5bedf05
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-11-10 02:55:00 -0800

    use const

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-//Global scope
-let board = [
+// Global scope
+const emptyBoard = [
   [, ,],
   [, ,],
   [, ,]
 ];
+let board = JSON.parse(JSON.stringify(emptyBoard));
 let tiles = document.querySelectorAll('.tile');
 let gameOver = false;
 let players = [];
@@ -13,30 +14,27 @@ let announcements = document.querySelector('.announcement');
 
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', () => {
-  announcements = document.querySelector('.announcement');
   announcements.textContent = '';
   resetBoard();
   restartGame();
   enableGameBoard();
 });
 
-function disableGameBoard() {
-  //add class disabled to container
+const disableGameBoard = () => {
   let box = document.querySelector('.container');
   box.classList.add('disabled');
-}
+};
 
-function enableGameBoard() {
+const enableGameBoard = () => {
   let box = document.querySelector('.container');
   box.classList.remove('disabled');
-}
+};
 
-function announce(text) {
+const announce = text => {
   announcements.textContent = text;
-}
+};
 
-function hasWinner() {
-  // Define the winning combinations using cell coordinates
+const hasWinner = () => {
   const winningCombo = [
     // Rows
     [
@@ -54,7 +52,6 @@ function hasWinner() {
       [2, 1],
       [2, 2]
     ],
-
     // Columns
     [
       [0, 0],
@@ -71,7 +68,6 @@ function hasWinner() {
       [1, 2],
       [2, 2]
     ],
-
     // Diagonals
     [
       [0, 0],
@@ -85,34 +81,27 @@ function hasWinner() {
     ]
   ];
 
-  // Check for a winner
   for (const combo of winningCombo) {
     const [a, b, c] = combo;
-    const [x1, y1] = a;
-    const [x2, y2] = b;
-    const [x3, y3] = c;
-
     if (
-      board[x1][y1] === currentPlayer.symbol &&
-      board[x2][y2] === currentPlayer.symbol &&
-      board[x3][y3] === currentPlayer.symbol
+      board[a[0]][a[1]] === currentPlayer.symbol &&
+      board[b[0]][b[1]] === currentPlayer.symbol &&
+      board[c[0]][c[1]] === currentPlayer.symbol
     ) {
       gameOver = true;
-
       announce(`${currentPlayer.name} wins!`);
       disableGameBoard();
       return true;
     }
   }
   return false;
-}
+};
 
-// Check for a tie
-function hasTie() {
+const hasTie = () => {
   for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 3; col++) {
       if (!board[row][col]) {
-        return false; // There is an empty cell, so it's not a tie yet
+        return false;
       }
     }
   }
@@ -120,16 +109,35 @@ function hasTie() {
   announce("It's a tie!");
   disableGameBoard();
   return true;
-}
+};
 
-//Game play logic
+const createPlayers = (name, symbol) => ({ name, symbol });
+
+const restartGame = () => {
+  players = [
+    createPlayers(document.querySelector('#player1').value, 'X'),
+    createPlayers(document.querySelector('#player2').value, 'O')
+  ];
+  currentPlayer = player1Turn ? players[0] : players[1];
+  announce(`It's ${currentPlayer.name}'s turn (${currentPlayer.symbol})`);
+  player1Turn = !player1Turn;
+};
+
+const resetBoard = () => {
+  board = JSON.parse(JSON.stringify(emptyBoard));
+  tiles.forEach(tile => {
+    tile.textContent = '';
+    tile.classList.remove('x-symbol', 'o-symbol');
+  });
+  gameOver = false;
+  currentPlayer = players[0];
+  player1Turn = true;
+};
+
 tiles.forEach((tile, index) => {
   tile.addEventListener('click', () => {
-    //update board array
     const row = Math.floor(index / 3);
     const col = index % 3;
-
-    //Checks for taken tiles, adds symbol
     if (!board[row][col]) {
       board[row][col] = currentPlayer.symbol;
       tile.textContent = currentPlayer.symbol;
@@ -140,45 +148,10 @@ tiles.forEach((tile, index) => {
       if (!hasWinner() && !hasTie()) {
         currentPlayer = player1Turn ? players[0] : players[1];
         announce(`It's ${currentPlayer.name}'s turn (${currentPlayer.symbol})`);
-        //switch turns
         player1Turn = !player1Turn;
       }
     }
   });
 });
-
-//Player logic
-const createPlayers = (name, symbol) => {
-  return { name, symbol };
-};
-
-function restartGame() {
-  players = [
-    createPlayers(document.querySelector('#player1').value, 'X'),
-    createPlayers(document.querySelector('#player2').value, 'O')
-  ];
-  currentPlayer = player1Turn ? players[0] : players[1];
-  //show who's turn it is in text
-  announce(`It's ${currentPlayer.name}'s turn (${currentPlayer.symbol})`);
-  //switch turns
-  player1Turn = !player1Turn;
-}
-
-function resetBoard() {
-  board = [
-    [, ,],
-    [, ,],
-    [, ,]
-  ];
-  // Remove symbols from tiles and reset their class
-  tiles.forEach(tile => {
-    tile.textContent = '';
-    tile.classList.remove('x-symbol', 'o-symbol');
-  });
-  // Reset game state
-  gameOver = false;
-  currentPlayer = players[0];
-  player1Turn = true;
-}
 
 restartGame();
